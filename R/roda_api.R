190164a89b2d500dcf8a93978f21927ac82ff1d0
Fernando <fptcorrea@gmail.com>
2024-04-17 18:47:58 -0300

    coloca as portas a mais

--- R/roda_api.R
+++ R/roda_api.R
@@ -1,3 +1,5 @@
 roda_api <- function(){
-  plumber::plumb(file='inst/apiModelo/plumber.R')$run()
+  plumber::plumb(file='inst/apiModelo/plumber.R')$run(
+    host = "0.0.0.0", port = 80, swagger = TRUE
+  )
 }
