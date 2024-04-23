2beab192f6787f14d471c5e7b861dd6e0fc8553e
Fernando <fptcorrea@gmail.com>
2024-04-22 21:41:56 -0300

    trocando para a porta 123

--- R/roda_api.R
+++ R/roda_api.R
@@ -1,5 +1,5 @@
 roda_api <- function(){
   plumber::plumb(file='inst/apiModelo/plumber.R')$run(
-    host = "0.0.0.0", port = 443, swagger = TRUE
+    host = "0.0.0.0", port = 123, swagger = TRUE
   )
 }
